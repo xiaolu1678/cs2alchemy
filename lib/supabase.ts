4dd596ea0157d50c14a1b2c2358c7d69e3c5517d
xiaolu167 <guzhengquan0720@gmail.com>
2026-04-02 13:15:11 +0800

    update

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -3,4 +3,12 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  auth: {
+    persistSession: true,
+    autoRefreshToken: true,
+    detectSessionInUrl: true,
+    storage: typeof window !== "undefined" ? window.sessionStorage : undefined,
+  },
+});
